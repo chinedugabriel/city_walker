8978744b98997d327385641735a7f5a9fd1a9d11
chinedugabriel <chinedugabriel29@gmail.com>
2022-08-09 23:56:12 +0100

    added an icon to the find buttin and added the API link for the map

--- city_walker.js
+++ city_walker.js
@@ -1,3 +1,17 @@
 // API Link  https://rapidapi.com/natkapral/api/countries-cities/
 
 // alert("working")
+
+testApi = async ()=>{
+    let url = "https://restcountries.com/v3.1/all";
+ 
+    let request = await fetch(url);
+    if (request.status === 200){
+        return requestrequest.json();
+    }throw "Not found"
+    // return request.json();
+};
+
+testApi().then(response => {
+    console.log(response)
+}).catch(error => console.log(error))
