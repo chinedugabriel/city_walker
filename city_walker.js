9b97e3302084d95a9caca215a561f08ed360b468
chinedugabriel <chinedugabriel29@gmail.com>
2022-08-13 05:02:09 +0100

    added scroll to the top button and scroll down aswell with icons

--- city_walker.js
+++ city_walker.js
@@ -13,7 +13,11 @@ let to_location = document.getElementById("to").value;
 
 function testInput(){// this function is to test the input tags
     
+    if(country === "" && city ==="" && from_location ===""&& to_location ===""){
+        alert("Please provide your destination")
+    }else{
     console.log(country +" "+ city +" "+ from_location +" "+ to_location);
+    }
 }
 
 
@@ -42,26 +46,24 @@ let findSectionForm = document.getElementById("find-section-form");
 mapDetailsSection.style.display === "none";
 
 let toggleView = ()=>{
-    // console.log(viewLocationButton.name)
-    if(mapDetailsSection.style.display === "none" ){
-        
 
-        findSectionForm.style.display = "none";
-        // mapDetailsSection.classList.add("place-center");
-        mapDetailsSection.style.display = "flex";
-        mapSection.style.display = "flex";
-        // viewLocationButton.name = false;
-        console.log(true);
-    }else{
-        mapDetailsSection.style.display = "none";
-        mapSection.style.display = "flex";
-        findSectionForm.style.display = "flex";
-        // viewLocationButton.name = true;
-        console.log(false);
-    }
+        // console.log(viewLocationButton.name)
+        if(mapDetailsSection.style.display === "none" ){
+            findSectionForm.style.display = "none";
+            // mapDetailsSection.classList.add("place-center");
+            mapDetailsSection.style.display = "flex";
+            mapSection.style.display = "flex";
+            // viewLocationButton.name = false;
+            console.log(true);
+        }else{
+            mapDetailsSection.style.display = "none";
+            mapSection.style.display = "flex";
+            findSectionForm.style.display = "flex";
+            console.log(false);
+        }
+    
 
 }
 
-// toggleView()
+toggleView();
 
-// add the .place-center class to the columns to position it properly
